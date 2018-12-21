0736583baf053615ee1fce42ca37ad41372a4af9
Issei Horie <issei.horie@nulab-inc.com>
2018-12-22 00:14:43 +0900

    Updated

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+// Sum returns sum of two integer values.
+func Sum(a, b int) int {
+	return a + b
+}
+
+func main() {
+
+}
---
+++ main_test.go
@@ -0,0 +1,7 @@
+package main
+
+import "testing"
+
+func Test_Sum(t *testing.T) {
+	Sum(1, 2)
+}
